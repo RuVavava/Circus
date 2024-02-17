aaa33f96b649c912fd2a2f68e4cb3fd3b8c349e5
Варя <varya.kornilova.21@inbox.ru>
2024-02-17 21:51:14 +0300

    Добавьте файлы проекта.

---
+++ Circus/DB/DBConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circus.DB
+{
+    internal class DBConnection
+    {
+        public static CircusEntities1 circus = new CircusEntities1(); //Строка подключения БД
+        public static Workers loginedWorker; //Строка вошедшего пользователя
+
+    }
+}
